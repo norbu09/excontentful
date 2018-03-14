b03d98306083e8578133519fb2e57191738209ba
Lenz Gschwendtner <lenz@springtimesoft.com>
2018-03-14 15:17:55 +1300

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Excontentful.MixProject do
   def project do
     [
       app: :excontentful,
-      version: "0.1.1",
+      version: "0.2.0",
       elixir: ">= 1.5.0",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
