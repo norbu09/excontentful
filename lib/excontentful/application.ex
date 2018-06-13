ff1a111e73a1ce9ee102f76ff4b30dbb492f1957
Lenz Gschwendtner <lenz@springtimesoft.com>
2018-06-14 04:06:00 +1200

    con_cache startup changed

--- lib/excontentful/application.ex
+++ lib/excontentful/application.ex
@@ -6,20 +6,16 @@ defmodule Excontentful.Application do
   use Application
 
   def start(_type, _args) do
-    import Supervisor.Spec, warn: false
-    # List all child processes to be supervised
-    children = [
-      # Starts a worker by calling: Excontentful.Worker.start_link(arg)
-      # supervisor(Excontentful, []),
-      supervisor(ConCache, [[
-        ttl_check_interval: :timer.seconds(1),
-        global_ttl: :timer.minutes(5)
-      ], [name: :content]])
-    ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Excontentful.Supervisor]
-    Supervisor.start_link(children, opts)
+    Supervisor.start_link([
+      {ConCache, [
+        ttl_check_interval: :timer.seconds(1),
+        global_ttl: :timer.minutes(5),
+        name: :content]}
+    ], 
+    opts)
   end
 end
